1cb82acccaaa04472e03ab0ada25c73f1ca66d68
Ulf Eliasson <eliassou@ituniv.se>
2007-02-24 00:09:24 +0100

    Improved tag managment... I think

--- src/wmii.ml
+++ src/wmii.ml
@@ -51,18 +51,15 @@ let dmenu out_str =
    let len = input c_in buffer 0 1014 in
    String.sub buffer 0 len
 
-let current_tags ?(ignore="") () =
+let current_tags () =
    let data = read conn rootfid "/tag" in
    let files = Ixpc.unpack_files data in
    List.fold_left 
    (
       fun name_list stat -> 
-         let name = stat.Fcall.name in
-         if (String.compare name ignore) == 0 then name_list
-         else
-            match stat.Fcall.name with
-            | "sel" -> []
-            | name -> name :: name_list 
+         match stat.Fcall.name with
+         | "sel" -> []
+         | name -> name :: name_list 
    ) [] files
 
 let client_tags () =
@@ -74,8 +71,24 @@ let current_tag () =
 let quit () =
    write conn rootfid "/ctl" "quit"
 
+let rec do_filtering l filter filtered =
+   match l with
+   | [] -> filtered
+   | head :: tail -> 
+      match String.compare head filter with
+      | 0 -> do_filtering tail filter filtered 
+      | _ -> do_filtering tail filter (head :: filtered)
+
+let rec filter l filter_l =
+   match filter_l with
+   | [] -> l
+   | head :: tail -> 
+         let new_l = do_filtering l head [] in
+         filter new_l tail
+
 (* Misc helper functions *)
-let list_to_str ?prefix:(pre = "") str_list =
+let list_to_str ?ignore:(ignore = []) ?prefix:(pre = "") str_list =
+   let filtered_list = filter str_list ignore in
    List.fold_left
    (
       fun str_list str ->
@@ -83,7 +96,7 @@ let list_to_str ?prefix:(pre = "") str_list =
          match str_list with
          | "" ->  pre_str 
          | _ -> str_list ^ "\n" ^ pre_str
-   ) "" str_list
+   ) "" filtered_list
 
 
 let read_dir dir = 
@@ -138,20 +151,19 @@ let view_tag tag =
    write conn rootfid "/ctl" ("view " ^ tag)
 
 let sel_tag _ =
-   let current = current_tag () in
-   let tags = current_tags ~ignore:current () in
-   let tags_str = list_to_str tags in
+   let current = [current_tag ()] in
+   let tags = current_tags () in
+   let tags_str = list_to_str ~ignore:current tags in
    let new_tag = dmenu tags_str in 
    view_tag new_tag
 
 let set_tag _ =
    try 
    let cid = read conn rootfid "/client/sel/ctl" in
-   let current = current_tag () in
    let client_tags = client_tags () in
-   let tags = current_tags ~ignore:current () in
+   let tags = current_tags () in
    let regular_tags = list_to_str tags in
-   let plus_tags = list_to_str ~prefix:"+" tags in
+   let plus_tags = list_to_str ~ignore:client_tags ~prefix:"+" tags in
    let minus_tags = list_to_str ~prefix:"-" client_tags in
    let tags_str =  minus_tags ^ "\n" ^ plus_tags ^ "\n" ^ regular_tags in
    let new_tag = dmenu tags_str in 
