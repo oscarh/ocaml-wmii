191bc7e96ab09fe749def67108a2696ba498ae34
Oscar Hellström <oscar@hellstrom.st>
2007-02-25 17:57:12 +0100

    Gajim must close input channels even if the command was unsucessful

--- plugin/gajim.ml
+++ plugin/gajim.ml
@@ -2,11 +2,9 @@
 let msg_count () =
     let cmd = "gajim-remote get_unread_msgs_number 2> /dev/null" in
     let chan = Unix.open_process_in cmd in
-    try
-        let count = input_line chan in
-        ignore (Unix.close_process_in chan);
-        count
-    with _ -> "-"
+    let count = try input_line chan with _ -> "-" in
+    ignore (Unix.close_process_in chan);
+    count
 
 let open_pending_event () =
     let cmd = "gajim-remote show_next_pending_event >/dev/null 2>&1 &" in
